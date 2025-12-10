b8be9465f3fc99c5f43ed70c8a30b1c9687e1390
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-10 20:11:42 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from db import get_db
+import crud
+import schemas
 
 app = FastAPI()
 
+# 기본 테스트용 API
 @app.get("/")
 def read_root():
     return {"msg": "pars-wms backend ok"}
+
+# 상품 등록 API
+@app.post("/products", response_model=schemas.ProductResponse)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    return crud.create_product(db, product)
