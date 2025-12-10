e70c14d1468c310dc7234ef675c88a01a2d07bdb
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-10 18:38:29 +0900

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,12 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Render 환경변수에서 DATABASE_URL 읽기
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
