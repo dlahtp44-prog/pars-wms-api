299fabda48aa4f22cbc6f6267522e1eca014f58f
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-11 08:18:19 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,1 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from db import get_db
-import crud
-import schemas
 
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"msg": "pars-wms backend ok"}
-
-@app.post("/products", response_model=schemas.ProductResponse)
-def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
-    return crud.create_product(db, product)
