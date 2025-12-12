9a0490fefb83334544f40b9b7bb73c91928baeca
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-12 11:44:33 +0900

    Update responsive.js

--- app/static/js/responsive.js
+++ app/static/js/responsive.js
@@ -1,1 +1,10 @@
 
+// 모바일 환경 최적화
+(function() {
+    console.log("Responsive UI Loaded");
+
+    // iOS 카메라 문제 해결
+    if (/iPhone|iPad/.test(navigator.userAgent)) {
+        document.body.style.height = "100%";
+    }
+})();
