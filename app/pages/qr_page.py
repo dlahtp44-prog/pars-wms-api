2761d607a3a05faa19777dc4ee3ddeb1107fd32c
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-12 11:36:21 +0900

    Add QR scan page route with HTML response

--- app/pages/qr_page.py
+++ app/pages/qr_page.py
@@ -1,1 +1,12 @@
 
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="app/templates")
+router = APIRouter(prefix="/qr", tags=["Page"], include_in_schema=False)
+
+
+@router.get("/scan", response_class=HTMLResponse)
+def qr_scan_page(request: Request):
+    return templates.TemplateResponse("qr_scan.html", {"request": request})
