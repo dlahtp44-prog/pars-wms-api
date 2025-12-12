e84f6130cac5397bc533cba1c54e92b338619101
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-12 11:34:20 +0900

    Add move_page route with HTML response

--- app/pages/move_page.py
+++ app/pages/move_page.py
@@ -1,1 +1,12 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="app/templates")
+router = APIRouter(prefix="/move", tags=["Page"], include_in_schema=False)
+
+
+@router.get("/", response_class=HTMLResponse)
+def move_page(request: Request):
+    return templates.TemplateResponse("move.html", {"request": request})
 
