175b026e37163b3d8f846d1436ff10e8149b9d5e
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-12 11:34:02 +0900

    Add outbound page route with HTML response

--- app/pages/outbound_page.py
+++ app/pages/outbound_page.py
@@ -1,1 +1,12 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="app/templates")
+router = APIRouter(prefix="/outbound", tags=["Page"], include_in_schema=False)
+
+
+@router.get("/", response_class=HTMLResponse)
+def outbound_page(request: Request):
+    return templates.TemplateResponse("outbound.html", {"request": request})
 
