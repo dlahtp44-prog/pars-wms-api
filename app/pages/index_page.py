95412a493f6d9d3d455089f9ab80eeea8f4cb1e2
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-12 11:33:32 +0900

    Add FastAPI router for index page

--- app/pages/index_page.py
+++ app/pages/index_page.py
@@ -1,1 +1,14 @@
 
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
+
+templates = Jinja2Templates(directory="app/templates")
+
+router = APIRouter(tags=["Page"], include_in_schema=False)
+
+
+@router.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
