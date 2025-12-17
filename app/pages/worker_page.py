e329988d3b03f30200530c437467dd132011f65b
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-17 21:56:48 +0900

    Update worker_page.py

--- app/pages/worker_page.py
+++ app/pages/worker_page.py
@@ -1,12 +1,49 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/worker",
+    tags=["작업자 화면"]
+)
+
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/worker/inbound")
+# =========================
+# 작업자 메인
+# =========================
+@router.get("")
+def worker_home(request: Request):
+    return templates.TemplateResponse(
+        "worker_home.html",
+        {"request": request}
+    )
+
+# =========================
+# 작업자 입고
+# =========================
+@router.get("/inbound")
 def worker_inbound(request: Request):
     return templates.TemplateResponse(
         "worker_inbound.html",
+        {"request": request}
+    )
+
+# =========================
+# 작업자 출고
+# =========================
+@router.get("/outbound")
+def worker_outbound(request: Request):
+    return templates.TemplateResponse(
+        "worker_outbound.html",
+        {"request": request}
+    )
+
+# =========================
+# 작업자 재고 이동
+# =========================
+@router.get("/move")
+def worker_move(request: Request):
+    return templates.TemplateResponse(
+        "worker_move.html",
         {"request": request}
     )
