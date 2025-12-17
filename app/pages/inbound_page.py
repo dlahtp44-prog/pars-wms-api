ec29d84e1e55314b1a18ae1ca6fbbcc7e544594b
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-17 15:19:36 +0900

    Update inbound_page.py

--- app/pages/inbound_page.py
+++ app/pages/inbound_page.py
@@ -5,11 +5,7 @@ router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get(
-    "/inbound-page",
-    summary="입고 화면",
-    description="모바일/PC용 입고 처리 화면"
-)
+@router.get("/inbound-page")
 def inbound_page(request: Request):
     return templates.TemplateResponse(
         "inbound.html",
