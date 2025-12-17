18f4b2b59a1d952d01e9151910e27d08c441d2d7
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-17 15:22:19 +0900

    Update inventory_page.py

--- app/pages/inventory_page.py
+++ app/pages/inventory_page.py
@@ -1,12 +1,19 @@
-
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/templates")
-router = APIRouter(prefix="/inventory", tags=["Page"], include_in_schema=False)
 
+router = APIRouter(
+    prefix="/inbound",
+    tags=["Page"],
+    include_in_schema=False
+)
 
 @router.get("/", response_class=HTMLResponse)
-def inventory_page(request: Request):
-    return templates.TemplateResponse("inventory.html", {"request": request})
+def inbound_page(request: Request):
+    return templates.TemplateResponse(
+        "inbound.html",
+        {"request": request}
+    )
+
