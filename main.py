b861f85c5f8cefe6bc8d87c65c26d836fe3d7054
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-17 14:34:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 
 app = FastAPI(
     title="PARS WMS",
-    description="물류 입·출고 및 재고 관리를 위한 WMS API",
+    description="물류 입·출고, 이동, 재고 관리를 위한 WMS API",
     version="1.0.0"
 )
 
@@ -15,7 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Routers
+# Router imports
 from app.routers.items import router as items_router
 from app.routers.inbound import router as inbound_router
 from app.routers.outbound import router as outbound_router
@@ -28,6 +27,7 @@ from app.routers.qr_api import router as qr_router
 from app.pages.index_page import router as index_router
 from app.pages.qr_page import router as qr_page_router
 
+# Router register
 app.include_router(index_router)
 app.include_router(items_router)
 app.include_router(inbound_router)
@@ -42,7 +42,7 @@ app.include_router(qr_page_router)
 @app.get(
     "/ping",
     summary="서버 상태 확인",
-    description="서버가 정상 동작 중인지 확인합니다."
+    description="서버가 정상적으로 동작 중인지 확인합니다."
 )
 def ping():
     return {"상태": "정상"}
