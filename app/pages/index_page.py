60a4d482d91c237612d8915bed240698b055b132
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-17 13:56:27 +0900

    Update index_page.py

--- app/pages/index_page.py
+++ app/pages/index_page.py
@@ -5,12 +5,13 @@ import os
 
 router = APIRouter()
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 templates = Jinja2Templates(directory=TEMPLATE_DIR)
 
 @router.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
