bc00c0c366a7c07940a757d2dee35b3caddb798a
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-17 09:29:34 +0900

    Update index_page.py

--- app/pages/index_page.py
+++ app/pages/index_page.py
@@ -1,12 +1,9 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/")
+@router.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse(
-        "dashboard.html",
-        {"request": request}
-    )
+    return templates.TemplateResponse("index.html", {"request": request})
