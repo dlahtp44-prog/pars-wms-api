0ad00c0c96e256d6aac674704664cc30566a56ab
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-18 13:23:53 +0900

    Update inventory_page.py

--- app/pages/inventory_page.py
+++ app/pages/inventory_page.py
@@ -10,12 +10,7 @@ def inventory_page(request: Request):
     conn = get_conn()
     rows = conn.execute("""
         SELECT
-            location_name,
-            brand,
             item_code,
-            item_name,
-            lot_no,
-            spec,
             location,
             qty
         FROM inventory
