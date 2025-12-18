330b22e7cd2c2d598e5a3745b28606d97622b95c
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-18 16:22:59 +0900

    Create upload_page.py

---
+++ app/pages/upload_page.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
+
+@router.get("/upload-page")
+def upload_page(request: Request):
+    return templates.TemplateResponse(
+        "upload.html",
+        {"request": request}
+    )
