2cd2b24032b52f89084b50bac17b4bb18ec9f857
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-19 08:36:17 +0900

    Update worker_page.py

--- app/pages/worker_page.py
+++ app/pages/worker_page.py
@@ -4,7 +4,6 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter(prefix="/worker")
 templates = Jinja2Templates(directory="app/templates")
 
-
 @router.get("")
 def worker_home(request: Request):
     return templates.TemplateResponse(
