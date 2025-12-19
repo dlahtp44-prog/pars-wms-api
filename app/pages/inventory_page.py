eb62f016ae35f8b57b8c08ceceabfbece1e90835
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-19 14:54:57 +0900

    Update inventory_page.py

--- app/pages/inventory_page.py
+++ app/pages/inventory_page.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from app.db import get_inventory
 
-router = APIRouter(prefix="/worker")
+router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/inventory")
+@router.get("/inventory-page")
 def inventory_page(request: Request):
     rows = get_inventory()
     return templates.TemplateResponse(
