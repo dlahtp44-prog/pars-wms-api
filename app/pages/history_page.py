650a3d211f1e326168dcd9a63379a294d764ff1b
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-22 08:56:06 +0900

    Update history_page.py

--- app/pages/history_page.py
+++ app/pages/history_page.py
@@ -7,8 +7,5 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/history-page")
 def history_page(request: Request):
-    rows = get_history()
-    return templates.TemplateResponse(
-        "history.html",
-        {"request": request, "rows": rows}
-    )
+    rows = get_history(200)
+    return templates.TemplateResponse("history.html", {"request": request, "rows": rows})
