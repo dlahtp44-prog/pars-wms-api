c803c1a074459f2703f3917c74b330b1c5761b53
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-22 08:54:21 +0900

    Update worker_page.py

--- app/pages/worker_page.py
+++ app/pages/worker_page.py
@@ -1,45 +1,9 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from app.db import get_unchecked_qr_error_count
 
 router = APIRouter(prefix="/worker")
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("")
 def worker_home(request: Request):
-    return templates.TemplateResponse(
-        "worker_home.html",
-        {
-            "request": request,
-            "qr_error_cnt": get_unchecked_qr_error_count()
-        }
-    )
-
-
-@router.get("/inbound")
-def worker_inbound(request: Request):
-    return templates.TemplateResponse(
-        "worker_inbound.html",
-        {"request": request}
-    )
-
-@router.get("/outbound")
-def worker_outbound(request: Request):
-    return templates.TemplateResponse(
-        "worker_outbound.html",
-        {"request": request}
-    )
-
-@router.get("/move")
-def worker_move(request: Request):
-    return templates.TemplateResponse(
-        "worker_move.html",
-        {"request": request}
-    )
-@router.get("/qr")
-def worker_qr(request: Request):
-    return templates.TemplateResponse(
-        "worker_qr.html",
-        {"request": request}
-    )
-
+    return templates.TemplateResponse("worker_home.html", {"request": request})
