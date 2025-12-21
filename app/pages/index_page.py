4bdf20a66d535b8d2e78ca4318962baecdd403b0
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-22 08:53:40 +0900

    Update index_page.py

--- app/pages/index_page.py
+++ app/pages/index_page.py
@@ -1,16 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from app.db import get_unchecked_qr_error_count
+from app.db import get_inventory
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/")
 def index(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
-            "qr_error_cnt": get_unchecked_qr_error_count()
-        }
-    )
+    rows = get_inventory()
+    return templates.TemplateResponse("index.html", {"request": request, "rows": rows})
