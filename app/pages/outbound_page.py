49c58cde8c78f901370049339578658c0dfa08bb
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-22 17:29:22 +0900

    Update outbound_page.py

--- app/pages/outbound_page.py
+++ app/pages/outbound_page.py
@@ -1,9 +1,13 @@
+# app/pages/outbound_page.py
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter(prefix="/worker")
+router = APIRouter(prefix="/outbound-page")
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/outbound")
-def worker_outbound(request: Request):
-    return templates.TemplateResponse("worker_outbound.html", {"request": request})
+@router.get("")
+def outbound_page(request: Request):
+    return templates.TemplateResponse(
+        "outbound.html",
+        {"request": request}
+    )
