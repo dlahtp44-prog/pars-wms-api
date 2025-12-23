ad15797c8bb2b4abb9d1ada676565692ee054597
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-23 18:38:01 +0900

    Update qr_page.py

--- app/pages/qr_page.py
+++ app/pages/qr_page.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(prefix="/qr-page")
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("", response_class=HTMLResponse)
+@router.get("")
 def qr_page(request: Request):
-    return templates.TemplateResponse("qr.html", {"request": request})
+    return templates.TemplateResponse("qr_page.html", {"request": request})
