b943c44e1ccb561f2208d24e35ce0f6cfafa5838
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-23 15:58:57 +0900

    Update sw.js

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,16 +1,2 @@
-const CACHE = "pars-wms-v1";
-const ASSETS = [
-  "/", "/dashboard", "/qr-page",
-  "/static/manifest.json",
-  "/static/qr_camera.js"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
