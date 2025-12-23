34e770fdce7bf219ac388f78e5bd569221cb9ebd
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-23 18:35:18 +0900

    Update index_page.py

--- app/pages/index_page.py
+++ app/pages/index_page.py
@@ -1,15 +1,9 @@
-# pages/index_page.py
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from app.db import get_history
 
 router = APIRouter()
-templates = Jinja2Templates("app/templates")
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/")
 def index(request: Request):
-    qr_error_cnt = sum(1 for h in get_history() if h["tx_type"] == "QR_ERROR")
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request, "qr_error_cnt": qr_error_cnt}
-    )
+    return templates.TemplateResponse("index.html", {"request": request})
