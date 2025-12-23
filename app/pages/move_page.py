03686778250607c90201da6ef03b1f501bcc62ee
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-23 18:36:45 +0900

    Update move_page.py

--- app/pages/move_page.py
+++ app/pages/move_page.py
@@ -1,4 +1,3 @@
-# app/pages/move_page.py
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
@@ -7,7 +6,4 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("")
 def move_page(request: Request):
-    return templates.TemplateResponse(
-        "move.html",
-        {"request": request}
-    )
+    return templates.TemplateResponse("worker_move.html", {"request": request})
