84706f7b9567c90b695d604973e725ba52398b43
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-24 18:47:45 +0900

    Update history.py

--- app/routers/history.py
+++ app/routers/history.py
@@ -1,12 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.db import get_history
 
 router = APIRouter(prefix="/api/history")
 
 @router.get("")
-async def read_history(limit: int = 200):
-    try:
-        data = get_history(limit=limit)
-        return data # 리스트 형태로 반환
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+def read_history(limit: int = 200):
+    return get_history(limit=limit)
