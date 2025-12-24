3214474db3b73e77e341d06d2543ff28075b548d
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-24 18:42:17 +0900

    Update history.py

--- app/routers/history.py
+++ app/routers/history.py
@@ -7,7 +7,6 @@ router = APIRouter(prefix="/api/history")
 async def read_history(limit: int = 200):
     try:
         data = get_history(limit=limit)
-        # 만약 데이터가 없으면 빈 리스트 [] 를 반환하여 에러를 방지합니다.
-        return data if data else []
+        return data # 리스트 형태로 반환
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
