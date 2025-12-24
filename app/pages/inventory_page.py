3b7378ac9867dcc04f4d06e6d5ca00c57a746d65
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-24 23:46:40 +0900

    Update inventory_page.py

--- app/pages/inventory_page.py
+++ app/pages/inventory_page.py
@@ -3,13 +3,12 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from app import db
 
-# 메인 앱에서 등록할 수 있도록 router 객체를 생성합니다.
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/inventory-page", response_class=HTMLResponse)
 async def inventory_page(request: Request, q: str = None):
-    # db.py의 검색 기능이 포함된 재고 조회 함수 호출
+    # db.py에서 검색 기능이 포함된 데이터를 가져옵니다.
     rows = db.get_inventory(q=q)
     
     return templates.TemplateResponse("inventory_page.html", {
