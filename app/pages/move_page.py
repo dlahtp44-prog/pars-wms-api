5b21fa636ac2353ad6cceef25a6a93f6cb5ca39d
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-24 20:49:11 +0900

    Delete app/pages/move_page.py

--- app/pages/move_page.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
-
-router = APIRouter(prefix="/move-page")
-templates = Jinja2Templates(directory="app/templates")
-
-@router.get("")
-def move_page(request: Request):
-    return templates.TemplateResponse("worker_move.html", {"request": request})
