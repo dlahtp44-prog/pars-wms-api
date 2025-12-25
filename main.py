06a527cc4d2f4b1459946af2b6f00a766039250f
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-25 18:03:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ safe_include("app.routers.qr_api")
 safe_include("app.routers.qr_process")
 safe_include("app.routers.export_excel")
 safe_include("app.routers.qr_generate")
+safe_include("app.routers.admin")
 
 @app.get("/ping")
 def ping():
