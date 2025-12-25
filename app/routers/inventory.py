142e3e69735a250b44b99bd7c20e57f4c992e459
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-25 10:52:09 +0900

    Update inventory.py

--- app/routers/inventory.py
+++ app/routers/inventory.py
@@ -1,8 +1,13 @@
+# app/routers/inventory.py
 from fastapi import APIRouter, Query
 from app.db import get_inventory
 
-router = APIRouter(prefix="/api/inventory")
+router = APIRouter(prefix="/api/inventory", tags=["inventory"])
 
 @router.get("")
-def inventory(q: str = Query("")):
-    return get_inventory(q=q)
+def inventory_api(
+    warehouse: str = Query(None),
+    location: str = Query(None),
+    q: str = Query(None),
+):
+    return get_inventory(warehouse=warehouse, location=location, q=q)
