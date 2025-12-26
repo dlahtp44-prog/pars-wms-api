32db4b6f8381b2c55114db4e538e7af3eef02ef5
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 12:30:58 +0900

    Update qr_label_page.py

--- app/pages/qr_label_page.py
+++ app/pages/qr_label_page.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/qr-label")
-def qr_label_page(request: Request):
+@router.get("/qr/label/location", response_class=HTMLResponse)
+def location_label(request: Request):
     return templates.TemplateResponse(
-        "qr_label.html",
+        "qr_label_location.html",
         {"request": request}
     )
 
+@router.get("/qr/label/item", response_class=HTMLResponse)
+def item_label(request: Request):
+    return templates.TemplateResponse(
+        "qr_label_item.html",
+        {"request": request}
+    )
