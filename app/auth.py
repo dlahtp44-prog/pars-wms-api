74777fe817f3b5920a1d46158794dc7a0989a1e8
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 11:01:02 +0900

    Delete app/auth.py

--- app/auth.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Request, HTTPException
-
-def require_admin(request: Request):
-    if request.session.get("is_admin") is True:
-        return True
-    raise HTTPException(status_code=401, detail="관리자 로그인 필요")
